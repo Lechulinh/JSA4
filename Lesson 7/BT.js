bd3db6248168c6d7e26d0752f65053b9f3605ce2
Lechulinh <Lechulinh>
2022-09-11 17:59:06 +0700

    hello

--- Lesson 7/BT.js
+++ Lesson 7/BT.js
@@ -5,17 +5,17 @@ const student = (name, age, address) => {
     console.log('dia chi:', address)
 }
 //Bai 2
-// var smartPhones = [ 
-//     { name: 'iphone', price: 649 }, 
-//     { name: 'Galaxy S6', price: 576 }, 
-//     { name: 'Galaxy Note 5', price: 489 } 
-// ];
-// function giaTien() {
-//     for (let x of smartPhones){
-//         console.log(x)
-//     }
-// }
-// giaTien()
+var smartPhones = [ 
+    { name: 'iphone', price: 649 }, 
+    { name: 'Galaxy S6', price: 576 }, 
+    { name: 'Galaxy Note 5', price: 489 } 
+];
+function giaTien() {
+    for (let x of smartPhones){
+        console.log(x.price)
+    }
+}
+giaTien()
 //Bai 3
 const foo1 = (x,y,z) => {
 	console.log( x, y, z );
